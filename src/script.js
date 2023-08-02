3bc5a8b9a5600b8f9a9fbc0db2e5282686a0b964
raziel <92149572+tiny-raziel@users.noreply.github.com>
2023-08-02 10:24:19 +0200

    Added Base File Structs

---
+++ src/script.js
@@ -0,0 +1,36 @@
+/*
+===HTML===
+Step One: Define Base Struct with aside and main
+-> aside has the th köln logo on top, and a button that says 'Add Message'
+-> aside includes small explanatory text about project
+-> main is the canvas
+Step Two: Revisit when Needed
+-> for javascript and everything
+===CSS===
+Step One: Define main and aside theme
+-> use th köln themed colours
+-> style button appropriately
+-> style explanatory message
+Step Two: Think about Message design
+-> style an example message
+-> test different character counts etc
+===JS===
+Step One: Functioning 'Add Message' Button
+-> it pops out an input window
+-> input happens
+Step Two: Transform input window into an uneditable element
+-> bg colour is one of three random th köln themed colours
+Step Three: Make it draggable
+-> difficult difficult
+-> maybe limit position to canvas??
+Step Four: Load and Read from LocalStorage
+-> just some tests
+Step Five: Load and Read Messages from LocalStorage
+-> optional name, message, colour
+-> position doesn't matter yet
+Step Six: Load and Position Messages according to LocalStorage
+-> good god
+Step Seven: Clear Messages Button
+-> optional, only if time
+-> scrubs the data clean baby
+*/
